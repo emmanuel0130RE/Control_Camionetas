06cbcb230fef4fb2ab4e9acc144921524d794ee7
Rolando Emmanuel Galvan Garza <135382703+emmanuel0130RE@users.noreply.github.com>
2026-05-13 16:43:09 -0600

    Delete reporte-camionetas directory

--- reporte-camionetas/config.credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Credenciales locales del módulo de camionetas.
-// Esta copia apunta a la BD de pruebas u650771697_EmmaBd.
-return [
-    'host'     => 'localhost',
-    'dbname'   => 'u650771697_EmmaBd',
-    'username' => 'u650771697_EmmaBd',
-    'password' => 'Rolis0130',
-];
--- reporte-camionetas/config.credentials.sample.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'host'     => 'localhost',
-    'dbname'   => 'TU_BASE_DE_DATOS',
-    'username' => 'TU_USUARIO',
-    'password' => 'TU_PASSWORD',
-];
